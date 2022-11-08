b2de67ef9277ca2ad8d14deda517ca44b578ecac
suhel khan <yinyingdark@gmail.com>
2022-11-08 20:52:59 +0530

    added new file

---
+++ BackEnd/Covid_map Project/index.js
@@ -0,0 +1,33 @@
+function updateMap() {
+  fetch("/data.json")
+    .then((response) => response.json())
+    .then((rsp) => {
+      // console.log(rsp.data)
+      rsp.data.forEach((element) => {
+        latitude = element.latitude;
+        longitude = element.longitude;
+
+          cases = element.infected;
+          if (cases>255){
+              color = "rgb(255, 0, 0)";
+          }
+
+          else{
+              color = `rgb(${cases}, 0, 0)`;
+          }
+
+        // Mark on the map
+        new mapboxgl.Marker({
+          draggable: true,
+            color: color
+        })
+          .setLngLat([longitude, latitude])
+          .addTo(map);
+
+          
+
+      });
+    });
+}
+
+updateMap();
